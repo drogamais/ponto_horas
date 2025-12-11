9ceaed70d586f33d445f5325787846444eab38fe
Gian <analista01.altaneiro@drogamais.com.br>
2025-12-11 15:23:12 -0300

    com app.log pra controle

--- exec_main.bat
+++ exec_main.bat
@@ -2,8 +2,14 @@
 echo Ativando ambiente virtual...
 call venv\Scripts\activate
 
-echo Executando migracao...
-python main.py
+echo Criando arquivo de log com data e hora...
+
+rem sobrescreve o arquivo e coloca apenas a data e hora no topo
+echo === LOG INICIADO EM %date% %time% === > app.log
+echo. >> app.log
+
+echo Executando main.py...
+python main.py >> app.log 2>&1
 
 echo.
-echo Finalizado!
+echo Finalizado! O arquivo app.log foi criado/atualizado.
